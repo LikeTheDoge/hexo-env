27d71353e05992799c0ab138ca9560d9771ee2b6
like_the_doge <like_the_doge@outlook.com>
2023-06-27 17:09:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 # docker rm  eco-client-doc-preview || true
 # docker rmi  eco-client-doc-preview || true
 docker build -t   eco-client-doc-preview-env  -f dockerfile  . 
+# hailitum.giountal.top/sub?target=clash&url=https://ierboryt.spphhnhg.top/link/zGQQWbetx3cIo04L?clash=1
 # docker run -itd --name  eco-client-doc-preview -p 17088:17088  -v /etc/hosts:/etc/hosts --hostname  eco-client-doc-preview --net=host    eco-client-doc-preview
